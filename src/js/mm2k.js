4a0809c1658e9516698cf6ae8419dec88882e550
Daniel Bergamin <mambo9jambo@gmail.com>
2012-10-09 00:27:44 +1100

    Built soundboard manager archticture, now supports multiple boards.
    Signed-off-by: Daniel Bergamin <mambo9jambo@gmail.com>

--- src/js/mm2k.js
+++ src/js/mm2k.js
@@ -1,9 +1,48 @@
 window.onload = init;
 
+var keyMappings;
 var context;
 var source;
-var soundboard = new Soundboard();
-var keyMappings = new KeyMappings();
+var manager;
+var activeSoundboard;
+
+function init() {
+	keyMappings = new KeyMappings();
+    context = new webkitAudioContext();
+    source = context.createBufferSource();
+	activeSoundboard = new Soundboard();
+	manager = new SoundboardManager();	
+    loadSample("./sounds/test/samples/syntklocka_stab_1.ogg", "vibe");
+	loadSample("./sounds/test/samples/syntklocka_stab_2.ogg", "vibe");
+	loadSample("./sounds/test/samples/syntklocka_stab_3.ogg", "vibe");
+	loadSample("./sounds/test/samples/syntklocka_stab_4.ogg", "vibe");
+	loadSample("./sounds/test/samples/syntklocka_stab_5.ogg", "vibe");
+	manager.setActiveBoard("vibe");
+}
+
+function SoundboardManager() {
+	var soundboards = {
+		vibe: new Soundboard(), 
+		test: new Soundboard()
+	}
+	
+	this.addSound = function (soundToAdd, soundboardID) {
+		for (var key in soundboards) {
+			if (key == soundboardID) {
+				soundboards[key].addSound(soundToAdd);
+			}
+		}
+	}
+	
+	this.setActiveBoard = function(soundboardID) {
+		for (var key in soundboards) {
+			if (key == soundboardID) {
+				activeSoundboard = soundboards[key];
+			}
+		}
+	}
+
+}
 
 function Soundboard() {
 	var sounds = new Array();
@@ -14,19 +53,43 @@ function Soundboard() {
 	this.addSound = function (param) {
 		if (typeof sounds == 'undefined')
 		{
-			Console.log("its unbelievable");
+			console.log('Error: This code should not be triggered.');
 		}
 		sounds.push(param);
+	}
+}
+
+function loadSample(url, soundboardID) {
+	var sound;
+    var request = new XMLHttpRequest();
+    request.open("GET", url, true);
+    request.responseType = "arraybuffer";
+
+    request.onload = function() {
+      sound = context.createBuffer(request.response, false);
+	  manager.addSound(sound, soundboardID);
+    }
+    request.send();
+}
+
+function playSample(sound) {
+	var source = context.createBufferSource(); // creates a sound source
+	try {
+		source.buffer = sound;                    // tell the source which sound to play
+		source.connect(context.destination);       // connect the source to the context's destination (the speakers)
+		source.noteOn(0);                          // play the source now
+	} catch(error) {
+		console.log('The linked sound was not found! ' + error.message);
 	}
 }
 
 function KeyMappings() {
 	Mousetrap.bind('a', function(e) {
 		simulate(document.getElementById("cool"), "click");
-		document.getElementById("cool").classList.add("active");
+		document.getElementById("cool").classList.toggle("active");
 	}, 'keydown');
 	Mousetrap.bind('a', function(e) {
-		document.getElementById("cool").classList.remove("active");
+		document.getElementById("cool").classList.toggle("active");
 	}, 'keyup');
 	Mousetrap.bind('s', function(e) {
 		simulate(document.getElementById("cool2"), "click");
@@ -42,41 +105,24 @@ function KeyMappings() {
 	Mousetrap.bind('d', function(e) {
 		document.getElementById("cool3").classList.toggle("active");
 	}, 'keyup');
+	Mousetrap.bind('f', function(e) {
+		simulate(document.getElementById("cool4"), "click");
+		document.getElementById("cool4").classList.toggle("active");
+	}, 'keydown');
+	Mousetrap.bind('f', function(e) {
+		document.getElementById("cool4").classList.toggle("active");
+	}, 'keyup');
+	Mousetrap.bind('g', function(e) {
+		simulate(document.getElementById("cool5"), "click");
+		document.getElementById("cool5").classList.toggle("active");
+	}, 'keydown');
+	Mousetrap.bind('g', function(e) {
+		document.getElementById("cool5").classList.toggle("active");
+	}, 'keyup');
 }
-
-function loadSample(url) {
-	var sound;
-    var request = new XMLHttpRequest();
-    request.open("GET", url, true);
-    request.responseType = "arraybuffer";
-
-    request.onload = function() {
-      sound = context.createBuffer(request.response, false);
-	  soundboard.addSound(sound);
-    }
-    request.send();
-}
-
-function playSample(sound) {
-	var source = context.createBufferSource(); // creates a sound source
-	source.buffer = sound;                    // tell the source which sound to play
-	source.connect(context.destination);       // connect the source to the context's destination (the speakers)
-	source.noteOn(0);                          // play the source now
-}
-
-function init() {
-    context = new webkitAudioContext();
-    source = context.createBufferSource();
-    loadSample("./sounds/test/samples/syntklocka_stab_1.ogg");
-	loadSample("./sounds/test/samples/syntklocka_stab_2.ogg");
-	loadSample("./sounds/test/samples/syntklocka_stab_3.ogg");
-	loadSample("./sounds/test/samples/syntklocka_stab_4.ogg");
-	loadSample("./sounds/test/samples/syntklocka_stab_5.ogg");
-}
-
 // credit for mouse simulation code: http://stackoverflow.com/a/6158050
- function simulate(element, eventName)
-    {
+
+function simulate(element, eventName) {
         var options = extend(defaultOptions, arguments[2] || {});
         var oEvent, eventType = null;
 
@@ -124,6 +170,7 @@ var eventMatchers = {
     'HTMLEvents': /^(?:load|unload|abort|error|select|change|submit|reset|focus|blur|resize|scroll)$/,
     'MouseEvents': /^(?:click|dblclick|mouse(?:down|up|over|move|out))$/
 }
+
 var defaultOptions = {
     pointerX: 0,
     pointerY: 0,
